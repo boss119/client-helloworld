065f64658d125c870ce4492935937b0665feb6ac
Warathat <boss119@hotmail.com>
2018-06-27 18:52:53 +0700

    Deploy on Heroku

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
 /* โหลด Express มาใช้งาน */
-var app = require('express')();
+var app = require('express');
 var add = require('./add');
 
 /* ใช้ port 7777 หรือจะส่งเข้ามาตอนรัน app ก็ได้ */
 var port = process.env.PORT || 7777;
 
 /* Routing */
-app.get('/', function (req, res) {
-    res.send('<h1>Hello Express Node.js</h1>');
-});
-
-app.get('/add', function (req, res) {
+app()
+  .get('/', (req, res) =>  res.send('<h1>Hello Heroku and Node.js</h1>'))
+  .get('/add', function (req, res) {
     let a = req.query.a;
     let b = req.query.b;
     let calculate = add
     res.send('<h1>a = ' + a + `</h1>` + `<br>` + '<h1>b = ' + b + `</h1>` + `<br>` + '<h1>Total = ' + calculate(a, b) + `</h1>`);
-    // res.send('<h1>Total = ' + calculate(a, b) + `</h1>`);
-});
+  })
+  .listen(port, () => console.log(`Listening on ${ port }`))
+
+// app.get('/', function (req, res) {
+//     res.send('<h1>Hello Heroku and Node.js</h1>');
+// });
+
+// app.get('/add', function (req, res) {
+//     let a = req.query.a;
+//     let b = req.query.b;
+//     let calculate = add
+//     res.send('<h1>a = ' + a + `</h1>` + `<br>` + '<h1>b = ' + b + `</h1>` + `<br>` + '<h1>Total = ' + calculate(a, b) + `</h1>`);
+// });
 
 /* สั่งให้ server ทำการรัน Web Server ด้วย port ที่เรากำหนด */
-app.listen(port, function() {
-    console.log('Starting node.js on port ' + port);
-});
+// app.listen(port, function() {
+//     console.log('Starting node.js on port ' + port);
+// });
