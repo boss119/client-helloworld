94798cb14009bb2d540fedcba615c4ac717972c3
Warathat <boss119@hotmail.com>
2018-07-16 10:09:57 +0700

    app build

---
+++ dist/precache-manifest.18089b3eeea2113a8c0688e229ba28cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "99e10174f999b960a669",
+    "url": "/js/chunk-vendors.99e10174.js"
+  },
+  {
+    "revision": "86091c527aad0a73d4db",
+    "url": "/js/app.86091c52.js"
+  },
+  {
+    "revision": "10ce9b7e7ab04b97ce736ab8ff43324f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "99e10174f999b960a669",
+    "url": "/css/chunk-vendors.f30bf4e0.css"
+  },
+  {
+    "revision": "86091c527aad0a73d4db",
+    "url": "/css/app.98bdd094.css"
+  }
+];
